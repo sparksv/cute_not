8bf93e51421914607ca61bdd57c96f911344a5c8
Valerie Sparks <valeriesparks@Valeries-MacBook-Pro.local>
2014-03-03 18:43:26 -0500

    created project added image scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CuteNot::Application.config.secret_key_base = '3228384f9a4a0ac759178c17f00cc428298af14b2a3a984635c2f77ae114f91eb010c62b0c068fa503aa2af175ed2e696a55724acf43459e4e42d0fbf48d0722'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CuteNot::Application.config.session_store :cookie_store, key: '_cute_not_session'
